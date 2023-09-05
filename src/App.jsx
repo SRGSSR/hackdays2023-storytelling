42b9201355a4ac27f00471280db7fd39d8db8335
Robert Salzer <robert.salzer@srf.ch>
2023-09-05 14:19:31 +0200

    ongoing work

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import { useState } from 'react'
 import './App.css'
+import {similar} from './utils/archipanion.js'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  const getSimilar = async(term) => {
+    const ids = await similar(term)
+    console.log(ids)
+  }
 
   return (
     <>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button onClick={() => getSimilar("Auto")}>Test API</button>
       </div>
     </>
   )
