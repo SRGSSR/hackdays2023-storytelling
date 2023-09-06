5f21b8b9e22b664dfe6cf2f70a3d3b327f487016
Sebastien Chauvin <sebastien.chauvin@rts.ch>
2023-09-06 11:44:35 +0200

    feat: proxy logs

--- api/serverless/proxy.js
+++ api/serverless/proxy.js
@@ -1,9 +1,10 @@
 import fetch from 'node-fetch';
 
-export default (request) => {
+export default async (request) => {
+    console.log('request', request);
     const maxResults = 1;
     const text = "Auto";
-    const content = fetch("https://srghackathon.archipanion.com/api/v1/find/segments/similar", {
+    const content = await fetch("https://srghackathon.archipanion.com/api/v1/find/segments/similar", {
         "headers": {
             "accept": "application/json, text/plain, */*",
             "content-type": "application/json",
@@ -11,7 +12,12 @@ export default (request) => {
         },
         "body": `{"config":{"maxResults":${maxResults}},"terms":[{"type":"TEXT","categories":["visualtextcoembedding"],"data":"${text}"}],"messageType":"Q_SIM"}`,
         "method": "POST",
-    }).then(r => r.json()).then(r => r.results[0].content)
+    }).then(r => {
+        console.log('result', r.status, r.statusText);
+        return r.json();
+    }).then(r => r.results[0].content)
+
+    console.log('content', content);
 
     return new Response(`Hello, ${content}`);
 };
