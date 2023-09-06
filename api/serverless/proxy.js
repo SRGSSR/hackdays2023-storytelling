9fc81e7438a8bc56fddd8fccf53d8e904fb9ca38
Sebastien Chauvin <sebastien.chauvin@rts.ch>
2023-09-06 11:55:23 +0200

    feat: search

--- api/serverless/proxy.js
+++ api/serverless/proxy.js
@@ -1,21 +1,71 @@
 import fetch from 'node-fetch';
 
+export const search = async (text, maxResults) => {
+    console.log("Starting Search...")
+    const similarResults = await similar(text, maxResults)
+    console.log(similarResults)
+    const keys = similarResults.map(r => r.key);
+    const idsResults = await ids(keys)
+    const objectIds = idsResults.map(i => i.objectId)
+    let os = await objects(objectIds);
+
+    idsResults.forEach((idResult, index) => {
+        const objectIdToSearchFor = idResult.objectId
+        const foundObject = os.find((object) =>
+            object.objectid === objectIdToSearchFor
+        )
+        idResult.path = foundObject.path;
+        idResult.score = similarResults[index].value;
+        idResult.term = text;
+    })
+    let filteredResult = idsResults.filter((r, index, array) => !array.slice(0, index).find(r2 => r2.path === r.path));
+    if (filteredResult.length !== idsResults.length) {
+        console.log(`Filtered Results from ${idsResults.length} to ${filteredResult.length}`);
+    }
+    return filteredResult;
+}
+
+const headers = {
+    "accept": "application/json, text/plain, */*",
+    "content-type": "application/json",
+    "authorization": "Basic c2VzYW06w7ZmZm5lZGljaA==",
+};
+export const similar = (text, maxResults = "10") =>
+    fetch("https://srghackathon.archipanion.com/api/v1/find/segments/similar", {
+        "headers": headers,
+        "body": `{"config":{"maxResults":${maxResults}},"terms":[{"type":"TEXT","categories":["visualtextcoembedding"],"data":"${text}"}],"messageType":"Q_SIM"}`,
+        "method": "POST",
+    }).then(r => r.json()).then(r => r.results[0].content);
+
+export const ids = (ids) => {
+    let body = `{"ids":[${ids.map(i => `"${i}"`).join(",")}]}`;
+    // console.log("body", body);
+    return fetch("https://srghackathon.archipanion.com/api/v1/find/segments/by/id", {
+        "headers": {
+            "accept": "application/json, text/plain, */*",
+            "authorization": "Basic c2VzYW06w7ZmZm5lZGljaA==",
+            "content-type": "application/json",
+        },
+        "body": body,
+        "method": "POST"
+    }).then(r => r.json()).then(r => r.content);
+};
+
+export const objects = (ids) => {
+    let body = `{"ids":[${ids.map(i => `"${i}"`).join(",")}]}`;
+    // console.log(body);
+    return fetch("https://srghackathon.archipanion.com/api/v1/find/object/by/id", {
+        "headers": headers,
+        "body": body,
+        "method": "POST"
+    }).then(r => r.json()).then(r => r.content);
+};
+
 export default async (request, res) => {
     console.log('request', request);
     const maxResults = 1;
     const text = "Auto";
-    const content = await fetch("https://srghackathon.archipanion.com/api/v1/find/segments/similar", {
-        "headers": {
-            "accept": "application/json, text/plain, */*",
-            "content-type": "application/json",
-            "authorization": "Basic c2VzYW06w7ZmZm5lZGljaA==",
-        },
-        "body": `{"config":{"maxResults":${maxResults}},"terms":[{"type":"TEXT","categories":["visualtextcoembedding"],"data":"${text}"}],"messageType":"Q_SIM"}`,
-        "method": "POST",
-    }).then(r => {
-        console.log('result', r.status, r.statusText);
-        return r.json();
-    }).then(r => r.results[0].content)
+    const content = search(text, maxResults);
 
     console.log('content', content);
 
