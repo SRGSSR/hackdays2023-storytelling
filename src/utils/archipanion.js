76094556aa50a85b21838b2fa6b18e37004c64ae
Sebastien Chauvin <sebastien.chauvin@rts.ch>
2023-09-06 12:06:15 +0200

    feat: api url

--- src/utils/archipanion.js
+++ src/utils/archipanion.js
@@ -2,5 +2,5 @@ export const search = async (text, maxResults) => {
     console.log("Starting Search...")
 
   return fetch(`/api/serverless/proxy?text=${text}&limit=${maxResults}`, {
-  }).then(r => r.json()).then(r => r.results[0].content)
+  }).then(r => r.json()).then(r => r.content)
 };
