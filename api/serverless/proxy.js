9677a887f97834c400f92dc35a5c8c48cd22deba
Robert Salzer <robert.salzer@srf.ch>
2023-09-06 12:41:46 +0200

    api for DEV-Mode

--- api/serverless/proxy.js
+++ api/serverless/proxy.js
@@ -72,5 +72,6 @@ export default async (request, res) => {
 
     res.statusCode = 200
     res.setHeader('Content-Type', 'application/json')
+    res.setHeader('Access-Control-Allow-Origin', '*')
     res.end(JSON.stringify({ content }));
 };
--- src/utils/archipanion.js
+++ src/utils/archipanion.js
@@ -1,6 +1,5 @@
 export const search = async (text, maxResults) => {
-    console.log("Starting Search...")
-
-  return fetch(`/api/serverless/proxy?text=${text}&limit=${maxResults}`, {
+  console.log("Starting Search...")
+  return fetch(`${import.meta.env.PROD ? '' : 'https://hackdays2023-storytelling.vercel.app'}/api/serverless/proxy?text=${text}&limit=${maxResults}`, {
   }).then(r => r.json()).then(r => r.content)
 };
